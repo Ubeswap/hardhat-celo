daca77ca230ba7fc86ea8d9313b387421251445c
Ian Macalinao <github@igm.pub>
2021-04-12 06:06:11 -0500

    Don't return void in doTx

--- src/utils/doTx.ts
+++ src/utils/doTx.ts
@@ -1,11 +1,12 @@
-import { ContractTransaction } from "ethers";
+import { ContractReceipt, ContractTransaction } from "ethers";
 import { log } from "./logger";
 
 export const doTx = async (
   action: string,
   tx: Promise<ContractTransaction>
-): Promise<void> => {
+): Promise<ContractReceipt> => {
   log(`Performing ${action}...`);
   const result = await (await tx).wait();
   log(`${action} done at tx ${result.transactionHash}`);
+  return result;
 };
