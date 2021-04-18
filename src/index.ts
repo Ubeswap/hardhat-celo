768c60c9bdd981b60bbaaecea2f4f24599eb189b
Ian Macalinao <github@igm.pub>
2021-04-18 02:09:19 -0500

    Add task for writing metadata

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,49 @@ import { CeloProvider, CeloWallet } from "@celo-tools/celo-ethers-wrapper";
 import { CeloContract, newKit } from "@celo/contractkit";
 import { BigNumber } from "ethers";
 import { HDNode } from "ethers/lib/utils";
-import { extendEnvironment } from "hardhat/config";
+import { extendEnvironment, task } from "hardhat/config";
 import { lazyObject } from "hardhat/plugins";
 import { HardhatNetworkHDAccountsConfig } from "hardhat/types";
 import { fornoURLs, parseNetwork } from "./networks";
+import { mkdir, writeFile } from "fs/promises";
 export * from "./networks";
 export * from "./type-extensions";
 export * from "./deployment";
 export * from "./utils";
 
 export const derivationPath = "m/44'/52752'/0'/0/";
+
+task<{ name: string }>(
+  "metadata:write",
+  "Writes the metadata of a contract to disk",
+  async ({ name }, hre) => {
+    const artifact = await hre.artifacts.readArtifact(name);
+    const fqn = `${artifact.sourceName}:${artifact.contractName}`;
+    const info = await hre.artifacts.getBuildInfo(fqn);
+    const contractBuildInfo =
+      info?.output.contracts[artifact.sourceName]?.[artifact.contractName];
+    const metadataStr = (contractBuildInfo as {
+      metadata?: string;
+    })?.metadata;
+    if (!metadataStr) {
+      throw new Error("metadata not found");
+    }
+    const contractDir = `build/metadata/${artifact.contractName}`;
+    await mkdir(contractDir, { recursive: true });
+
+    const metadataFile = `${contractDir}/metadata.json`;
+    await writeFile(metadataFile, metadataStr);
+    console.log("Wrote metadata to " + metadataFile);
+
+    const contractInfoFile = `${contractDir}/bytecode.json`;
+    const cleanBuildInfo = {
+      bytecode: contractBuildInfo?.evm.bytecode.object,
+      deployedBytecode: contractBuildInfo?.evm.deployedBytecode.object,
+    };
+    await writeFile(contractInfoFile, JSON.stringify(cleanBuildInfo));
+    console.log("Wrote contract bytecode to " + contractInfoFile);
+  }
+).addParam("name", "The name of the contract");
 
 extendEnvironment((hre) => {
   hre.celo = lazyObject(() => {
